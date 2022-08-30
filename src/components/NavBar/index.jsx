78c266917efbe53a2d3d1424b13245672d0e05c5
Gonzalo Celan <gonzaloezequielcelan@hotmail.com>
2022-08-29 22:57:16 -0300

    desafioNavbar

---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import "./style.css";
+
+export const NavBar = () => {
+  return (
+    <div>
+        <header>
+            <div className='logo'>
+                <h1>MANGO</h1>
+            </div>
+            <nav>
+                <ul>
+                    <a href="#">
+                        <li>HOMBRE</li>
+                    </a>
+                    <a href="#">
+                        <li>MUJERES</li>
+                    </a>
+                    <a href="#">
+                        <li>ACCESORIOS</li>
+                    </a>
+                    <a href="#">
+                        <li>SALE</li>
+                    </a>
+                    
+                </ul>
+            </nav>
+        </header>
+
+
+    </div>
+  )
+}
